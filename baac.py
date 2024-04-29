329a0778f4558cbff62d7839b046f2718ad627ae
Bird-PC <bird_aries@hotmail.com>
2024-04-29 21:15:47 +0700

    go throw folder ywd

--- baac.py
+++ baac.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from utils import create_web_driver , move_files
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from datetime import datetime, timedelta
 
 import time
 import logging
@@ -40,6 +41,15 @@ def logout(driver):
     time.sleep(3)
 
 def payment_l001(driver):
+
+    current_time = time.time()
+    this_year = formatted_date = time.strftime("%Y", time.localtime(current_time))
+    this_ym = formatted_date = time.strftime("%Y%m", time.localtime(current_time))
+
+    today = datetime.now()
+    yesterday = today - timedelta(days=1)
+    this_ymd = yesterday.strftime("%Y%m%d")
+
     menu = driver.find_element(By.ID, "repo_chooser")  # driver.find_element(By.XPATH, "//a[@href='#']")
 
     # Create an ActionChains object
@@ -53,7 +63,53 @@ def payment_l001(driver):
     span_payment = driver.find_element(By.XPATH, "//span[text()='payment[L001]']")
     span_payment.click()
 
-#TODO : Click year month day , and download file
+    time.sleep(15)
+
+    # TODO : Click year month day , and download file
+    css_id_row_year = "item-" + this_year
+    css_row_year = driver.find_element(By.ID, css_id_row_year)
+    actions.double_click(css_row_year).perform()
+
+    time.sleep(10)
+
+    css_id_row_ym = "item-" + this_ym
+    css_row_ym = driver.find_element(By.ID, css_id_row_ym)
+    actions.double_click(css_row_ym).perform()
+
+    time.sleep(10)
+
+    # vertical_splitter
+    # content_pane
+    # Option 2: Scroll to the element before clicking
+
+#TODO : connot find id css
+    css_id_row_ymd = "item-"+this_ymd
+    css_row_ymd = driver.find_element(By.ID, css_id_row_ymd)
+    # driver.execute_script("arguments[0].scrollIntoView();", css_row_ymd)
+    # time.sleep(3)
+    # css_row_ymd.click()
+    time.sleep(3)
+    actions.double_click(css_row_ymd).perform()
+
+    time.sleep(10)
+    css_download = driver.find_element(By.ID, "download_button_label")
+
+    css_id_row_dc106 = "item-" + this_ymd + "dc106l001" + this_ymd +"pdf"
+    css_row_dc106 = driver.find_element(By.ID, css_id_row_dc106)
+    css_row_dc106.click()
+    css_download.click()
+
+    time.sleep(5)
+
+    css_id_row_dc105 = "item-" + this_ymd + "dc105l001" + this_ymd + "pdf"
+    css_row_dc105 = driver.find_element(By.ID, css_id_row_dc105)
+    css_row_dc105.click()
+    css_download.click()
+
+
+
+
+
 
 def main():
     username = "ghb"
