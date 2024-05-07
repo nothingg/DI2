a7f2bc978370e3ffee7ecc3818770b75bb8507c8
bird GHB <bird_aries@hotmail.com>
2024-05-07 15:13:40 +0700

    mPay - 1

--- mpay.py
+++ mpay.py
@@ -41,20 +41,6 @@ def login(driver):
         logging.error(f"mPay Service: An error occurred: {str(e)}", exc_info=True)
         sys.exit(1)  # Exit the program with an error code
 
-# def login(driver, username, password):
-#     # Perform login with provided credentials
-#     driver.get("https://mpaystation.ais.co.th/AISPayStationGenWeb/authenUser?command=start")
-#
-#     input_username = driver.find_element(By.NAME, "username")
-#     input_password = driver.find_element(By.NAME, "password")
-#     login_button = driver.find_element(By.NAME, "login")
-#
-#     input_username.send_keys(username)
-#     input_password.send_keys(password)
-#     time.sleep(3)
-#
-#     login_button.click()
-
 def logout(driver):
     try :
         # Logout from the system
@@ -67,12 +53,6 @@ def logout(driver):
     except Exception as e:
         logging.error(f"mPay Service: An error occurred: {str(e)}", exc_info=True)
         sys.exit(1)  # Exit the program with an error code
-
-# def logout(driver):
-#     # Logout from the system
-#     logout_link = driver.find_element(By.XPATH, "//a[@href='/AISPayStationGenWeb/index.jsp']")
-#     logout_link.click()
-#     time.sleep(3)
 
 def download_xmlfile(driver, input_date):
     try :
@@ -115,38 +95,6 @@ def download_xmlfile(driver, input_date):
         logging.error(f"mPay Service: An error occurred: {str(e)}", exc_info=True)
         sys.exit(1)  # Exit the program with an error code
 
-# def download_xmlfile(driver):
-#     today = datetime.now()
-#     yesterday = today - timedelta(days=1)
-#     txt_dated_yesterday = yesterday.strftime("%Y%m%d")
-#
-#     main_menu = driver.find_element(By.XPATH, "//a[@href='#']")
-#
-#     # Create an ActionChains object
-#     actions = ActionChains(driver)
-#
-#     # Perform mouse hover action on the main UL element
-#     actions.move_to_element(main_menu).perform()
-#     time.sleep(3)
-#
-#     xml_menu = driver.find_element(By.XPATH, "//a[@href='/AISPayStationGenWeb/report']")
-#     xml_menu.click()
-#     time.sleep(10)
-#
-#     xml_link = driver.find_element(By.XPATH, f"//input[contains(@onclick, 'AIS{txt_dated_yesterday}.xml')]")
-#     xml_link.click()
-#     time.sleep(10)
-#
-# #TODO : if PDF file can't donwload , it's download manual
-#     # Switch to the new tab
-#     driver.switch_to.window(driver.window_handles[1])
-#
-#     pdf_link = driver.find_element(By.XPATH, f"//a[contains(@onclick, 'AIS{txt_dated_yesterday}.xml')]")
-#
-#     pdf_link.click()
-#     time.sleep(3)
-
-
 def download_txtfile(driver,input_date):
     try :
         input_date_obj = datetime.strptime(input_date, '%Y-%m-%d')
@@ -177,34 +125,6 @@ def download_txtfile(driver,input_date):
         logging.error(f"mPay Service: An error occurred: {str(e)}", exc_info=True)
         sys.exit(1)  # Exit the program with an error code
 
-# def download_txtfile(driver):
-#
-#     today = datetime.now()
-#     yesterday = today - timedelta(days=1)
-#     txt_dated_yesterday = yesterday.strftime("%Y%m%d")
-#
-#
-#     main_menu = driver.find_element(By.XPATH, "//a[@href='#']")
-#
-#     # Create an ActionChains object
-#     actions = ActionChains(driver)
-#
-#     # Perform mouse hover action on the main UL element
-#     actions.move_to_element(main_menu).perform()
-#     time.sleep(3)
-#
-#     txt_menu = driver.find_element(By.XPATH, "//a[@href='/AISPayStationGenWeb/text']")
-#     txt_menu.click()
-#     time.sleep(3)
-#
-#     # Download files using WebDriver
-#
-#     # txt_link = driver.find_element(By.XPATH, f"//a[@href='javascript:loadFile(\'AIS{txt_dated_yesterday}.log\');']")
-#     xpath_expression = f"//a[contains(@href, 'AIS{txt_dated_yesterday}.log')]"
-#     txt_link = driver.find_element(By.XPATH, xpath_expression)
-#     txt_link.click()
-#     time.sleep(3)
-
 def main():
     input_date = "2024-05-01"
     try:
@@ -225,6 +145,7 @@ def main():
         print('error : ' + str(e))
         logging.error(f"An error occurred in mPay function: {str(e)}", exc_info=True)
         sys.exit(1)  # Exit the program with an error code
+    # ไม่ต้องมี finally เนื่องจาก load pdf ไม่ได้ ต้อง manual load
     # finally:
     #     driver.quit()
 
