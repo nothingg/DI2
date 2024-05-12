dce860791b5859c467b2b071a7d25d7c47b3f3b2
Bird-PC <bird_aries@hotmail.com>
2024-05-12 14:50:49 +0700

    demo SFTP

--- demo.py
+++ demo.py
@@ -1,27 +1,70 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import paramiko
+from utils import create_web_driver,move_files, servu_download
 
-#สร้าง Web Driver
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach",True)
-driver = webdriver.Chrome(options=options)
 
-#Download
-driver.get("file:///E:/my_work_OLD/_Git/Python/DI2/html/Counter%20Service%20Co.,Ltd.html")
-current_time = time.time()  # Get current time in seconds since epoch
-# formatted_date = '261' + time.strftime("%m%d", time.localtime(current_time))
-formatted_date = time.strftime("%m%d", time.localtime(current_time))
+# Server details
+ip_server = "test.rebex.net"
+username = "demo"
+password = "password"
+port = 22
+server_path = "/pub/example/"
+filename = "imap-console-client.png"
 
-filezip = "downloadclientfile.asp?file=GHB\\"+formatted_date + '%2Ezip'
-a_txt = "a[href='"+filezip+"']"
+# Local path (ensure it exists)
+local_path = "C:/Downloads/"
 
+
+servu_download("20250501",server_path,local_path,"MEA")
+#
+# try:
+#     # Create SSH transport
+#     transport = paramiko.Transport((ip_server, port))
+#     transport.connect(username=username, password=password)
+#
+#     # Create SFTP client
+#     sftp = paramiko.SFTPClient.from_transport(transport)
+#
+#     # Remote file path
+#     remote_file = server_path + filename
+#
+#     # Local file path (including filename)
+#     local_file = local_path + filename
+#
+#     # Download the file
+#     sftp.get(remote_file, local_file)
+#
+#     # Close connections
+#     sftp.close()
+#     transport.close()
+#
+#     print(f"File downloaded successfully: {local_file}")
+# except Exception as e:
+#     print(f"An error occurred: {e}")
+
+#
+# #สร้าง Web Driver
+# options = webdriver.ChromeOptions()
+# options.add_experimental_option("detach",True)
+# driver = webdriver.Chrome(options=options)
+#
+# #Download
+# driver.get("file:///E:/my_work_OLD/_Git/Python/DI2/html/Counter%20Service%20Co.,Ltd.html")
+# current_time = time.time()  # Get current time in seconds since epoch
+# # formatted_date = '261' + time.strftime("%m%d", time.localtime(current_time))
+# formatted_date = time.strftime("%m%d", time.localtime(current_time))
+#
+# filezip = "downloadclientfile.asp?file=GHB\\"+formatted_date + '%2Ezip'
+# a_txt = "a[href='"+filezip+"']"
+#
 
 # Find the anchor tag using CSS selector (recommended)
 # link_to_click = driver.find_element(By.CSS_SELECTOR, "a[href='https://counterservice.co.th/ticketnet/clients/downloadclientfile.asp?file=GHB\\2610405%2Ezip']")
 
 # link_to_click = driver.find_element(By.XPATH, "//a[@href='https://counterservice.co.th/ticketnet/clients/downloadclientfile.asp?file=GHB\\2610405%2Ezip']")
-link_to_click = driver.find_element(By.XPATH, "//a[@href='https://counterservice.co.th/ticketnet/clients/downloadclientfile.asp?file=GHB\\261"+formatted_date+"%2Ezip']")
+# link_to_click = driver.find_element(By.XPATH, "//a[@href='https://counterservice.co.th/ticketnet/clients/downloadclientfile.asp?file=GHB\\261"+formatted_date+"%2Ezip']")
 
 # Click the link
 #link_to_click.click()
--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import time
 import os
 import shutil
 import logging
+import paramiko
+
 from selenium import webdriver
 
 
@@ -27,3 +29,63 @@ def move_files(source_dir, destination_dir):
     except Exception as e:
         print('error : ' + str(e))
         logging.error(f"An error occurred in function: move_files: {str(e)}")
+
+
+def servu_download(input_date, server_path, local_path,biller):
+    # Server details
+    ip_server = "test.rebex.net"
+    username = "demo"
+    password = "password"
+    port = 22
+    filename_IN = "mail"
+    # server_path = "DCR/Counter/IN/"
+    # filename = "20240509_counter.txt"
+    #
+    # # Local path (ensure it exists)
+    # local_path = "D:/Counter/20240509/"
+    try:
+        # Create SSH transport
+        transport = paramiko.Transport((ip_server, port))
+        transport.connect(username=username, password=password)
+
+        # Create SFTP client
+        sftp = paramiko.SFTPClient.from_transport(transport)
+
+        # Change directory to the server path
+        sftp.chdir(server_path)
+
+        # List files in the directory
+        files = sftp.listdir()
+
+        # Filter files based on the pattern
+        files_to_download = [file for file in files if file.startswith("INDCRxxxxxx" + input_date ) and file.endswith(".txt")]
+
+        # Download each file
+        for filename in files_to_download:
+            # Remote file path
+            remote_file = server_path + filename
+
+            # Local file path (including filename)
+            local_file = local_path + filename
+
+            # Download the file
+            sftp.get(remote_file, local_file)
+
+            print(f"File downloaded successfully: {local_file}")
+
+        if(biller == "lotus"):
+            true_files_to_download = [file for file in files if file.startswith("REDCRxxxxxx" + input_date) and file.endswith(".zip")]
+            for filename_zip in true_files_to_download:
+                remote_file_zip = server_path + filename_zip
+                local_file_zip = local_path + filename_zip
+                sftp.get(remote_file_zip, local_file_zip)
+
+                print(f"File Zip downloaded successfully: {local_file_zip}")
+
+        # Close connections
+        sftp.close()
+        transport.close()
+
+    except Exception as e:
+        print('error : ' + str(e))
+        logging.error(f"An error occurred in function: servu_download: {str(e)}")
